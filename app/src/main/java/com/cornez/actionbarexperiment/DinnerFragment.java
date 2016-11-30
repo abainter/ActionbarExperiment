86d446e58721d7195ae399572739381e69cd4282
Andrew <andrewbainter13@augustana.edu>
2016-11-29 21:08:38 -0600

    Initial commit

---
+++ app/src/main/java/com/cornez/actionbarexperiment/DinnerFragment.java
@@ -0,0 +1,20 @@
+package com.cornez.actionbarexperiment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ver on 11/29/2016.
+ */
+
+public class DinnerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        //Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dinner, container, false);
+    }
+}
